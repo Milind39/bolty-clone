a56251481fb4b3c8a1cfb4a26e39658fe9e49d9e
Milind Pawar <milindpawar1639@gmail.com>
2026-07-06 12:28:09 +0530

    signinlogic

--- bolty/components/NavBar/nav.tsx
+++ bolty/components/NavBar/nav.tsx
@@ -2,13 +2,7 @@
 
 import Link from "next/link";
 import React from "react";
-import {
-  SignInButton,
-  SignOutButton,
-  SignedIn, // Corrected
-  SignedOut, // Corrected
-  UserButton,
-} from "@clerk/nextjs";
+import { SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 
 const Nav = () => {
   return (
@@ -27,23 +21,21 @@ const Nav = () => {
           </Link>
 
           {/* Corrected: Use SignedOut to show login button */}
-          <SignedOut>
-            <SignInButton mode="modal">
-              <button className="text-gray-200 hover:text-blue-400 font-medium transition cursor-pointer">
-                Sign In
-              </button>
-            </SignInButton>
-          </SignedOut>
+
+          <SignInButton mode="modal">
+            <button className="text-gray-200 hover:text-blue-400 font-medium transition cursor-pointer">
+              Sign In
+            </button>
+          </SignInButton>
 
           {/* Corrected: Use SignedIn to show user profile and logout */}
-          <SignedIn>
-            <UserButton />
-            <SignOutButton>
-              <button className="text-gray-200 hover:text-blue-400 font-medium transition cursor-pointer">
-                Sign Out
-              </button>
-            </SignOutButton>
-          </SignedIn>
+
+          <UserButton />
+          <SignOutButton>
+            <button className="text-gray-200 hover:text-blue-400 font-medium transition cursor-pointer">
+              Sign Out
+            </button>
+          </SignOutButton>
         </div>
       </nav>
     </div>
