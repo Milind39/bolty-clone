218ac3d1ca0c627c776045636898ba34a76af4ee
Milind Pawar <milindpawar1639@gmail.com>
2026-07-06 11:41:47 +0530

    base

--- bolty/app/page.tsx
+++ bolty/app/page.tsx
@@ -133,8 +133,9 @@ export default function Home() {
       </div>
 
       <footer className="w-full text-center text-gray-300 text-sm pt-1">
-        <hr className="border-gray-600 my-8" />© 2025 StackBlitz - All rights
-        reserved.
+        <hr className="border-gray-600 my-8" />
+        Built with 💻 - Crafted with precision - Powered by caffeine ☕ — by
+        Milind
       </footer>
     </main>
   );
